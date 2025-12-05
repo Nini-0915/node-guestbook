959e39f9505037de38f8c67d50a1f03acb1545af
英文名 <>
2025-12-05 09:26:54 +0800

    1205

---
+++ public/script.js
@@ -0,0 +1,9 @@
+function over(element) {
+    // 當滑鼠進入時，將文字設為粗體
+    element.style.fontWeight = 'bold';
+}
+
+function out(element) {
+    // 當滑鼠離開時，恢復文字為正常字體
+    element.style.fontWeight = 'normal';
+}
